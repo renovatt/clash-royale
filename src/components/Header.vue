da0fa97b18c9d8792478b367f173cbf018e89036
will <wlymes@gmail.com>
2023-12-24 15:53:16 -0300

    feat: header

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="sticky left-0 top-0 z-50 flex w-full items-center justify-between bg-900 p-6">
+    <h2 class="text-3xl text-600 drop-shadow-contour">Clash Royale</h2>
+    <input type="search" placeholder="Mini P.E.K.K.A"
+      class="rounded-lg p-2 py-4 text-xs outline-none placeholder:text-xs">
+  </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
