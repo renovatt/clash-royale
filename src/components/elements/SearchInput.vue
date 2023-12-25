0007df3f9140b9f581407346a2cd90f3c933d782
will <wlymes@gmail.com>
2023-12-25 13:55:46 -0300

    fear: header

---
+++ src/components/elements/SearchInput.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <input type="search" placeholder="Mini P.E.K.K.A" class="rounded-lg p-2 py-4 text-xs outline-none placeholder:text-xs">
+</template>
+
---
+++ src/components/modules/Header.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import SearchInput from '../elements/SearchInput.vue';
+</script>
+
+<template>
+  <header
+    class="sticky -top-1 left-0 z-50 flex w-full flex-col items-center justify-between gap-6 bg-900 p-6 md:flex-row">
+    <figure class="h-24">
+      <img class="h-full" src="../../assets/img/logo_clashroyale.webp" alt="logo">
+    </figure>
+    <search-input />
+  </header>
+</template>
+
