b624cfb291200a939fa34817f0476d99f1804a6c
will <wlymes@gmail.com>
2023-12-25 13:54:52 -0300

    feat: styles updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -5,8 +6,30 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        900: '#002442',
+        800: '#0689fc',
+        700: '#ffab18',
+        600: '#f8f8f8',
+        500: '#888888',
+        400: '#abb8c2',
+        "common": "#1e1e1e",
+        "rare": "#ff6600",
+        "epic": "#cc00ff",
+        legendary: "#efbdff",
+        champion: "#f3d40d"
+      },
+      dropShadow:{
+        'contour': '2px 3px 0px rgba(0, 0, 0, 1)',
+        'smooth': '0 2px rgba(0, 0, 0, 0.15)',
+      },
+      backgroundImage:{
+        modal: "url('/src/assets/img/bg_modal.jpg')",
+        modal_desk: "url('/src/assets/img/bg_modal_desk.jpg')",
+      }
+    },
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar-hide')],
 }
 
