56bc7f20c8857ca17df0a8a8951ae1e4d48e331e
will <wlymes@gmail.com>
2023-12-26 18:14:59 -0300

    refactor: router links refactored

--- src/components/modules/Header.vue
+++ src/components/modules/Header.vue
@@ -1,17 +1,32 @@
 <script setup lang="ts">
+import { Menu } from 'lucide-vue-next';
 import { RouterLink } from 'vue-router';
-import SearchInput from '../elements/SearchInput.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Cards'
+  },
+  {
+    path: '/challenges',
+    name: 'Challenges'
+  }
+]
 </script>
 
 <template>
-  <header class="sticky left-0 top-0 z-50 flex w-full flex-col items-center justify-between gap-6 bg-900 p-4 md:flex-row">
-    <figure class="h-16">
+  <header class="sticky inset-0 z-50 flex w-full items-center justify-between gap-6 bg-900 p-4 md:flex-row">
+    <figure class="h-20">
       <img class="h-full" src="../../assets/img/logo_clashroyale.webp" alt="logo">
     </figure>
 
-    <RouterLink to="/" class="text-600">Cards</RouterLink>
-    <RouterLink to="/challenges" class="text-600">Challenges</RouterLink>
-    <search-input />
+    <Menu class="h-10 w-10 text-600 md:hidden" />
+
+    <nav class="hidden items-center justify-center gap-5 md:flex">
+      <RouterLink v-for="route in routes" :key="route.path" :to="route.path" active-class="text-700"
+        class="text-2xl text-600 drop-shadow-contour transition-all ease-in-out hover:text-700">{{ route.name }}
+      </RouterLink>
+    </nav>
   </header>
 </template>
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,20 +3,22 @@ import Home from '../views/Home.vue'
 
 const lazyLoad = (view: string) => import(`../views/${view}.vue`)
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/challenges',
+    name: 'challenges',
+    component: lazyLoad('Challenges')
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/challenges',
-      name: 'challenges',
-      component: lazyLoad('Challenges')
-    },
-  ]
+  routes
 })
 
 export default router
