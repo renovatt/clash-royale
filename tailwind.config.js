ee612d91ab83d73fa94946738d9f47a77e389c2b
will <wlymes@gmail.com>
2023-12-26 17:28:54 -0300

    feat: updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   ],
   theme: {
     extend: {
-      colors:{
+      colors: {
         900: '#002442',
         800: '#0689fc',
         700: '#ffab18',
@@ -20,17 +20,42 @@ export default {
         legendary: "#efbdff",
         champion: "#f3d40d"
       },
-      dropShadow:{
+      dropShadow: {
         'contour': '2px 3px 0px rgba(0, 0, 0, 1)',
         'smooth': '0 2px rgba(0, 0, 0, 0.15)',
       },
-      backgroundImage:{
+      backgroundImage: {
         modal: "url('/src/assets/img/bg_modal.jpg')",
         modal_desk: "url('/src/assets/img/bg_modal_desk.jpg')",
         challenge: "url('/src/assets/img/hero_bg_clashroyale.webp')",
+
+        'gradient-common': 'linear-gradient(to top, #1e1e1e, #002442)',
+        'gradient-rare': 'linear-gradient(to top, #ff6600, #002442)',
+        'gradient-epic': 'linear-gradient(to top, #cc00ff, #002442)',
+        'gradient-legendary': 'linear-gradient(to top, #efbdff, #002442)',
+        'gradient-champion': 'linear-gradient(to top, #f3d40d, #002442)',
       }
     },
   },
+  safelist: [
+    'bg-common',
+    'bg-rare',
+    'bg-epic',
+    'bg-legendary',
+    'bg-champion',
+
+    'text-common',
+    'text-rare',
+    'text-epic',
+    'text-legendary',
+    'text-champion',
+
+    'border-common',
+    'border-rare',
+    'border-epic',
+    'border-legendary',
+    'border-champion'
+  ],
   plugins: [require('tailwind-scrollbar-hide')],
 }
 
