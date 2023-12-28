8f9f0d75e6e3afe317d72a5ea417b38256e19691
will <wlymes@gmail.com>
2023-12-28 14:33:52 -0300

    style: container removed

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
-import Container from './components/elements/Container.vue';
 import Footer from './components/modules/Footer.vue';
 import Header from './components/modules/Header.vue';
 </script>
 
 <template>
-  <!-- <Container> -->
   <Header />
   <main class="min-h-screen">
     <RouterView />
   </main>
   <Footer />
-  <!-- </Container> -->
 </template>
